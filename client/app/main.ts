51cdc0ba0a071e2bea4bd517b6aae97cbc60d83e
SNNJM <sitinurnazhifahjm@gmail.com>
2017-02-13 10:01:37 +0800

    Add files via upload

---
+++ client/app/main.ts
@@ -0,0 +1,6 @@
+//this is just to bootstrap our app core components
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
